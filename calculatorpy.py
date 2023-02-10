a4b1c20bcef2b6940e6dc3e1245d31073d109b48
vasinghal128 <125060733+vasinghal128@users.noreply.github.com>
2023-02-11 01:19:26 +0530

    Add files via upload

---
+++ calculatorpy.py
@@ -0,0 +1,23 @@
+a = float(input("Enter your first number"))
+
+b = float(input("Enter your second number"))
+
+sum = a+b
+
+subtract = a-b
+
+multiply = a-b
+
+devide = a/b
+
+devide2 = a//b
+
+print(sum)
+
+print(subtract)
+
+print(multiply)
+
+print(devide)
+
+print(devide2)
---
+++ voating.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age"))
+
+if(age>18):
+   print("You are eligible for voating")
+
+else:
+    print("You are not eligible for voating")
+
+print("Thank you")
